79730c7746c279a63bf18012b651377939a8c532
kriswangsaputra <kriswangsaputra04@gmail.com>
2025-09-20 17:57:55 +0700

    section galeri

--- js/style.js
+++ js/style.js
@@ -12,3 +12,13 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
+
+// overlay
+const title = document.querySelector('.galery-title');
+
+document.querySelector('.galery').addEventListener('mouseenter', function(){
+  title.style.display = "block";
+})
+document.querySelector('.galery').addEventListener('mouseleave', function(){
+  title.style.display = "none";
+})
