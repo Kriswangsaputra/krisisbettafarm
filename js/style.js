ade19547eb6350f99f7ed2701a7f0fc19c405f87
kriswangsaputra <kriswangsaputra04@gmail.com>
2025-10-02 18:16:56 +0700

    editing responsif

--- js/style.js
+++ js/style.js
@@ -12,3 +12,12 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
+
+const btnTogle = document.getElementById('navbar-togle');
+const navMenu = document.getElementById('nav-list-id');
+const navContainer = document.getElementById('navbar-container-id');
+
+btnTogle.addEventListener('click',() =>{
+  navMenu.classList.toggle('show');
+  navContainer.classList.toggle('show');
+});
